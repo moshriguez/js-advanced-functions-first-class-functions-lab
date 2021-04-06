f39d60d2e8aab726a68053cfe31fc1521b77dfd2
Marc Rodriguez <marc.herman.rodriguez@gmail.com>
2021-04-06 10:33:44 -0700

    Completed

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Code your solution in this file!
+const drivers = ['Sally', 'Bob', 'Freddy', 'Claudia'];
+
+const returnFirstTwoDrivers = function(arr) {
+    return arr.slice(0, 2)
+}
+
+const returnLastTwoDrivers = function(arr) {
+    return arr.slice(-2);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const fareDoubler = fare => fare * 2;
+const fareTripler = fare => fare * 3;
+const fareQuadrupler = fare => fare * 4;
+const fareQuintupler = fare => fare * 5;
+
+function createFareMultiplier(num) {
+    switch (num) {
+        case 2:
+            return fareDoubler;
+        case 3:
+            return fareTripler;
+        case 4:
+            return fareQuadrupler;
+        case 5:
+            return fareQuintupler;
+    }
+}
+
+function selectDifferentDrivers(arr, func) {
+    return func(arr);
+}
